1105752fe9f8a626f4dd3dfeeeb73ebff5b390b2
Cameron <cameron.cartwright122804@gmail.com>
2024-06-04 19:42:58 +0100

    Handles Events
    Creates a function which handles the mouse event

--- portfolio/src/components/ListGroup.tsx
+++ portfolio/src/components/ListGroup.tsx
@@ -1,19 +1,27 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Newcastle", "Manchester", "Bournemouth", "Poole", "Sunderland"];
-
-  items = [];
 
   const getMessage = () => {
     return items.length === 0 && <p>No iten found</p>;
   };
+
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
