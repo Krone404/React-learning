861a3c0b61bd6a6a6522eeb6d854c1f1e30b520a
Cameron <cameron.cartwright122804@gmail.com>
2024-06-04 17:32:23 +0100

    Condition for rendering

--- portfolio/src/components/ListGroup.tsx
+++ portfolio/src/components/ListGroup.tsx
@@ -1,15 +1,16 @@
 function ListGroup() {
-  const items = [
-    "Newcastle",
-    "Manchester",
-    "Bournemouth",
-    "Poole",
-    "Sunderland",
-  ];
+  let items = ["Newcastle", "Manchester", "Bournemouth", "Poole", "Sunderland"];
+
+  items = [];
+
+  const getMessage = () => {
+    return items.length === 0 && <p>No iten found</p>;
+  };
 
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
